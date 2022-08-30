ae48fd6e651e234381b431eed81a9a761dc21fdf
Kauan Rocha <kau.matosr@gmail.com>
2022-08-30 13:05:45 -0300

    feat: header first steps

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled'
+import { SxProps, Theme } from '@mui/material'
+
+export const temp_logoIcon: SxProps<Theme> = {
+    color: 'primary.main'
+}
+
+export const temp_logoText: SxProps<Theme> = {
+    ml: 2,
+    mr: 2,
+    fontFamily: 'monospace',
+    fontWeight: 700,
+    letterSpacing: '.3rem',
+    color: 'inherit',
+    textDecoration: 'none',
+    textTransform: 'uppercase'
+}
+
+export const Spacer = styled.span`
+    flex: 1 1 auto;
+`
+
+export const Buttons = styled.div`
+    display: flex;
+    gap: 1rem;
+`
