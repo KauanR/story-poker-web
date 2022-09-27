b13dd12ba067a8b0f881e67366925d49a83a7ea4
KauanR <kau.matosr@gmail.com>
2022-09-27 19:30:00 -0300

    feat: deadling with environment variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,7 @@ module.exports = {
         ],
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/no-empty-function': 'off'
+        '@typescript-eslint/no-empty-function': 'off',
+        'import/no-anonymous-default-export': 'off'
     }
 }
