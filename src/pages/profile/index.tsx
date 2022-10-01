55df3513b56b1e58eff44c44d9c2e20e07db22cc
KauanR <kau.matosr@gmail.com>
2022-10-01 18:18:36 -0300

    feat: profile page component

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import type { NextPage } from 'next'
 import Profile from '../../components/pages/profile'
 import useAuthRedirect from '../../hooks/useAuthRedirect'
+import { useUser } from '../../hooks/useUser'
+import { CircularProgress } from '@mui/material'
 
 const ProfilePage: NextPage = () => {
     useAuthRedirect()
 
-    return <Profile />
+    const { user } = useUser()
+
+    return user ? <Profile user={user} /> : <CircularProgress sx={{margin: 'auto'}}/>
 }
 
 export default ProfilePage
